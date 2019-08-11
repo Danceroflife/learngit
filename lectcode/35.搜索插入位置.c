535fd6598e2330d137ae6596161bcbe78cd848f0
LiTing <32817408+Danceroflife@users.noreply.github.com>
2019-08-11 09:58:33 +0800

    Create 35.搜索插入位置.c

---
+++ lectcode/35.搜索插入位置.c
@@ -0,0 +1,33 @@
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+你可以假设数组中无重复元素。
+
+示例 1:
+
+输入: [1,3,5,6], 5
+输出: 2
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left=0;
+        int right=nums.size()-1;
+        int mid;
+        
+        while(left<=right)
+        {
+            mid=(right+left)/2;
+            
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]>target)
+                right=mid-1;
+            else
+                left=mid+1;
+        }
+        if(nums[mid]>target)
+            return mid;
+        return mid+1;
+        
+    }
+};
